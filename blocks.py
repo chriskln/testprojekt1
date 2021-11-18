7e98ee4525dde9f611efcbce8d7528940558d6d0
chriskln <klein.chris97@web.de>
2021-11-18 18:02:17 +0100

    blocks with conditions and loops

---
+++ blocks.py
@@ -0,0 +1,19 @@
+a = 10
+
+
+for i in range(3):
+    print(i)
+    print(a)
+    b = 10
+
+print('a=%s' % a)
+print('b=%s' % b)
+
+
+if a>5:
+    if b > 6:
+        print('a>5 and b>6')
+        c = 30
+else:
+    c=50
+print('c=%s' % c)
