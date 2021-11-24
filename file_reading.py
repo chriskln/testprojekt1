555ce784eace1545024ec321f54ae46a143d5fa1
chriskln <klein.chris97@web.de>
2021-11-24 16:29:47 +0100

    basic file reading

---
+++ file_reading.py
@@ -0,0 +1,21 @@
+file_path = 'text.txt'
+
+# 'r' is the mode to open the file (r = open for reading)
+# 'b' = open files in binary mode
+
+with open(file_path, 'r') as my_file:
+    lines = my_file.readlines()
+    i = 1
+    for line in lines:
+        print('{}: {}'.format(i, line), end='')
+        i+=1
+
+#my_file = open(file_path, 'r')
+#lines = my_file.readline()
+#i = 1
+#for line in lines:
+#        print('{}: {}'.format(i, line), end='')
+#        i+=1
+#my_file.close()
+
+
