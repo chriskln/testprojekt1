4021f0bfdc59c50b4fb3403df26796e46417f7f3
chriskln <klein.chris97@web.de>
2021-12-13 22:48:24 +0100

    files directories

---
+++ file_directory.py
@@ -0,0 +1,23 @@
+import os
+
+print(__file__) # unix way of representing a file path
+
+my_file_path = __file__
+
+my_parent_dir = os.path.dirname(my_file_path)
+my_file_name = os.path.basename(my_file_path)
+print(my_parent_dir)
+print(my_file_name)
+
+# in_path = os.path.join(os.getcwd(), 'data', '.keep') # join directory with a file
+
+in_path = os.path.join(os.getcwd(), 'data', '.keep')
+
+print(in_path)
+
+print(os.listdir(my_parent_dir))# printing list of all files I have
+#print(os.listdir('c:\\Users')) # printing list of users
+
+# my_dir = 'c\\Users\\klein' # kann user 'klein' nicht finden
+#for f in os.listdir(my_dir):
+ #   print(f)
