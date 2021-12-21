e136afc5a8e03fe0e165bf4bd12512014844e5d2
chriskln <klein.chris97@web.de>
2021-12-21 10:44:07 +0100

    Summing space occupied by folder and files

---
+++ space_count.py
@@ -0,0 +1,16 @@
+import os
+
+my_dir = 'C:\\Temp\\t'
+
+def sum_space(folder):
+    sum = 0
+    for f in os.listdir(folder): # f will return names of files
+        #type = "d" if os.path.isdir(os.path.join(my_dir, f)) else "-"
+        if os.path.isfile(os.path.join(my_dir, f)):
+            size = os.path.getsize(os.path.join(my_dir, f))
+            sum = sum + size
+        return sum
+
+print("Space occupied by {}: {}".format(my_dir, sum_space(my_dir)))
+
+# like this, it does not take subfolders into account
