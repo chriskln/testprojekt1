715020b0ac0ab91696bf30de015d3fc1e5457205
chriskln <klein.chris97@web.de>
2021-12-27 10:20:36 +0100

    space count adjustments

--- space_count.py
+++ space_count.py
@@ -21,11 +21,13 @@ def sum_space(folder):
     sum = 0
     for f in os.listdir(folder): # f will return names of files
         #type = "d" if os.path.isdir(os.path.join(my_dir, f)) else "-"
-        if os.path.isfile(os.path.join(folder, f)):
+        if os.path.isdir(os.path.join(folder, f)):
+            sum = sum + sum_space(os.path.join(folder, f))
+        else:
             size = os.path.getsize(os.path.join(folder, f))
             sum = sum + size
-        else:
-            sum += sum_space(os.path.join(folder, f))
+        #else:
+        #    sum += sum_space(os.path.join(folder, f))
         return sum
 
 print("Space occupied by {}: {}".format(my_dir, sum_space(my_dir)))
